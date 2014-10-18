3b190f7dfd9df1c42f231e0c7e4a7be39c78231d
Richard Siwady <rsiwady29@gmail.com>
2014-10-18 10:33:43 -0600

    remove commented code

--- Classes/MainMenu.cpp
+++ Classes/MainMenu.cpp
@@ -4,24 +4,14 @@ USING_NS_CC;
 
 CCScene* MainMenu::scene()
 {
-    // 'scene' is an autorelease object
     CCScene *scene = CCScene::create();
-    
-    // 'layer' is an autorelease object
     MainMenu *layer = MainMenu::create();
-
-    // add layer as a child to scene
     scene->addChild(layer);
-
-    // return the scene
     return scene;
 }
 
-// on "init" you need to initialize your instance
 bool MainMenu::init()
 {
-    //////////////////////////////
-    // 1. super init first
     if ( !CCLayer::init() )
     {
         return false;
@@ -29,26 +19,6 @@ bool MainMenu::init()
     
     CCSize visibleSize = CCDirector::sharedDirector()->getVisibleSize();
     CCPoint origin = CCDirector::sharedDirector()->getVisibleOrigin();
-
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
-
-    // add a "close" icon to exit the progress. it's an autorelease object
-    CCMenuItemImage *pCloseItem = CCMenuItemImage::create(
-                                        "CloseNormal.png",
-                                        "CloseSelected.png",
-                                        this,
-                                        menu_selector(MainMenu::menuCloseCallback));
-    
-	pCloseItem->setPosition(ccp(origin.x + visibleSize.width - pCloseItem->getContentSize().width/2 ,
-                                origin.y + pCloseItem->getContentSize().height/2));
-
-    // create menu, it's an autorelease object
-    CCMenu* pMenu = CCMenu::create(pCloseItem, NULL);
-    pMenu->setPosition(CCPointZero);
-    this->addChild(pMenu, 1);
-
     
     return true;
 }
--- Classes/MainMenu.h
+++ Classes/MainMenu.h
@@ -6,16 +6,10 @@
 class MainMenu : public cocos2d::CCLayer
 {
 public:
-    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
-    virtual bool init();  
-
-    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    virtual bool init();
     static cocos2d::CCScene* scene();
-    
-    // a selector callback
     void menuCloseCallback(CCObject* pSender);
     
-    // implement the "static node()" method manually
     CREATE_FUNC(MainMenu);
 };
 
