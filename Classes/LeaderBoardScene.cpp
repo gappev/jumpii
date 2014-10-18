926c8e12d9091cdc4bfd8a4191b799743f673f5c
Richard Siwady <rsiwady29@gmail.com>
2014-10-18 12:52:06 -0600

    adding files back again

---
+++ Classes/LeaderBoardScene.cpp
@@ -0,0 +1,43 @@
+#include "MainMenuScene.h"
+
+USING_NS_CC;
+
+Scene* MainMenuScene::createScene()
+{
+    auto scene = Scene::create();
+    auto layer = MainMenuScene::create();
+    scene->addChild(layer);
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool MainMenuScene::init()
+{
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    
+    
+    
+    return true;
+}
+
+
+void MainMenuScene::menuCloseCallback(Ref* pSender)
+{
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
+	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
+    return;
+#endif
+
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
